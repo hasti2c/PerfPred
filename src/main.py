e378561b98bd1891a7d900343425fde8b3c7fa08
Hasti Toossi <hasti.toossi@mail.utoronto.ca>
2023-08-31 10:13:34 -0400

    moved file

--- src/main.py
+++ src/main.py
@@ -3,15 +3,12 @@ import warnings
 import experiment.assess as A
 import experiment.info as I
 # import experiment.analyze as A
-import experiment.run as R
 import experiment.setup as S
 import util as U
 
-from modeling.model import Model as M
-import modeling.functions as F
-from modeling.trial import Trial as T
 from slicing.variable import Variable as V
+import experiment.run as R
+from modeling.trial import Trial as T
 
-model = M.get_instance(F.logarithmic, 1)
-trial = T([V.TRAIN_SIZE], [V.LANG], model)
-fits, costs, kfs = trial.fit()
+R.run_on_trials(lambda trial: print(trial.path))
+R.run_on_trials(T.fit, splits_list=[[], [V.LANG]])
